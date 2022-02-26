d85bb5abb5ad527fbf6115cbb656ed3f0b7bb247
vocaltech <it@vocaltech.fr>
2022-02-26 18:50:13 +0100

    refactor contextLoads()

--- src/test/java/fr/vocaltech/fsm/server/ServerApplicationTests.java
+++ src/test/java/fr/vocaltech/fsm/server/ServerApplicationTests.java
@@ -1,13 +1,18 @@
 package fr.vocaltech.fsm.server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-class ServerApplicationTests {
+public class ServerApplicationTests {
+	@Autowired
+	private ServerController controller;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
-
 }
