adf8c4bf58212541cc1bb209b38549664c0d81e9
vocaltech <it@vocaltech.fr>
2022-02-26 19:06:11 +0100

    refactor

--- src/test/java/fr/vocaltech/fsm/server/BasicControllerTest.java
+++ src/test/java/fr/vocaltech/fsm/server/BasicControllerTest.java
@@ -1,23 +1,26 @@
 package fr.vocaltech.fsm.server;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BasicControllerTest {
-    	/*
+    @LocalServerPort
+    int port;
+
 	@Autowired
 	private TestRestTemplate restTemplate;
 
-	 */
-
-    	/*
 	@Test
 	public void indexShouldReturnDefaultMessage() {
-		String url = "http://localhost:8080";
+		String url = "http://localhost:" + port;
 		assertThat(restTemplate.getForObject(url, String.class)).contains("ServerController works !");
 	}
-	*/
 
 }
