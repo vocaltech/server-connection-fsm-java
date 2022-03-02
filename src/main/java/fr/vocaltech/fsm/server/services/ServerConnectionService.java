75bc65d1936354fe757cc77c3a343e15b3e118a3
vocaltech <it@vocaltech.fr>
2022-03-02 21:42:20 +0100

    add checkHealth() method

--- src/main/java/fr/vocaltech/fsm/server/services/ServerConnectionService.java
+++ src/main/java/fr/vocaltech/fsm/server/services/ServerConnectionService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -42,11 +44,31 @@ public class ServerConnectionService {
         //String url =
     }
 
-    // TODO: checkHealth()
     public String checkHealth() {
         String url_server_health = env.getProperty("url.server.health");
 
-        System.out.println("url_server_health: " + url_server_health);
+        try {
+            URL url = new URL(url_server_health);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setDoOutput(true);
+            con.setConnectTimeout(500);
+
+            // read the response
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            final StringBuilder res = new StringBuilder();
+
+            String inputLine;
+            while ( (inputLine = br.readLine()) != null ) {
+                res.append(inputLine).append("\n");
+            }
+            br.close();
+
+            return(res.toString());
+
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
 
         return "";
     }
